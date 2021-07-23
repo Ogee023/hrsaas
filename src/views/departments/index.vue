3dc048458ada8d6e8bf0ff5e71396c8457c63bfe
Ogee023 <572516040@qq.com>
2021-07-23 16:03:36 +0800

    完成了角色和公司模块

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dashboard-container">
+  <div v-loading="loading" class="dashboard-container">
     <div class="app-container">
       <!-- 组织架构内容 头部 -->
       <el-card class="tree-card">
@@ -44,7 +44,8 @@ export default {
         label: 'name' // 表示从这个属性显示内容
       },
       showDialog: false, // 默认不显示
-      node: null // 记录当前点击的node节点
+      node: null, // 记录当前点击的node节点
+      loading: false // 用来控制进度弹层的显示和隐藏
     }
   },
   created() {
@@ -52,11 +53,13 @@ export default {
   },
   methods: {
     async getDepartments() {
+      this.loading = true
       const result = await getDepartments()
       this.company = { name: result.companyName, manager: '负责人', id: '' } // 如果不加，最外层的添加找不到id触发不了校验规则
       // 需要将其转化成树形结构
       this.departs = tranListToTreeData(result.depts, '')
-      console.log(result)
+      // console.log(result)
+      this.loading = false
     },
     // 监听tree-tools中触发的点击添加子部门的事件,node是我们当前点击的部门
     addDepts(node) {
