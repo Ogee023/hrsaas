fdf97ed66f1ddaa8b62bc0b6d45d307c82e134e4
十一 <572516040@qq.com>
2021-08-13 13:46:00 +0800

    完成了优化

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,8 @@ import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
 import ScreenFull from './ScreenFull'
 import ThemePicker from './ThemePicker'
+import LangSelect from './Lang'
+import TagsView from './TagsView'
 import Print from 'vue-print-nb'
 export default {
   install(Vue) {
@@ -13,6 +15,8 @@ export default {
     Vue.component('ImageUpload', ImageUpload) // 注册图片上传组件
     Vue.component('ScreenFull', ScreenFull)
     Vue.component('ThemePicker', ThemePicker)
+    Vue.component('LangSelect', LangSelect)
+    Vue.component('TagsView', TagsView)
     Vue.use(Print)
   }
 }
